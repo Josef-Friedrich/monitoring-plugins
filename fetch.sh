f42cdb5563d070903278e60bfea5240df348c12b
Josef Friedrich <josef@friedrich.rocks>
2018-04-07 20:41:56 +0200

    Update the monitoring plugins

--- fetch.sh
+++ fetch.sh
@@ -1,5 +1,11 @@
 #! /bin/sh
 
-wget https://raw.githubusercontent.com/Josef-Friedrich/check_unattended_upgrades/master/check_unattended_upgrades
+_get() {
+        wget -O $1 \
+        https://raw.githubusercontent.com/JosefFriedrich-shell/$1/master/$1
+        chmod a+x $1
+}
 
-chmod a+x check_unattended_upgrades
+_get check_unattended_upgrades
+_get check_zfs_snapshot
+_get check_zpool_scrub
